2fcb3c2d0d1ade237db2f0aff7c24b37feea0581
Koral <korali1995@gmail.com>
2023-11-01 12:12:47 +0200

    successfully

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Registrations from "./components/studentRegistrations/Registrations";
+import NewStudent from './components/newStudent/NewStudent';
+import { getAllStudents, getAllCourses, createNewRegisteredStudent } from './services/api';
+import normalizeCourseName from './utils/courseUtils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [registeredStudents, setRegisteredStudents] = useState([]);
+    const [availableCourses, setAvailableCourses] = useState();
+
+    useEffect(() => {
+        getAllCourses().then(
+            res => {
+                const courses = res.data.map(course => {
+                    const courseName = normalizeCourseName(course.name);
+                    return ({...course, displayName: courseName})
+                });
+                getAllStudents().then(
+                    res => {
+                        const students = res.data.map(student => {
+                            const studentName = student.firstName + " " + student.lastName;
+                            return ({...student, studentName: studentName})
+                        });
+                        setAvailableCourses(courses);
+                        setRegisteredStudents(students);
+                    })
+            }
+        );
+    }, []);
+
+    const addStudentHandler = (registeredStudent) => {
+        const studentNameSplit = registeredStudent.studentName.split(" ");
+        const studentFirstName = studentNameSplit[0];
+        const studentLastName = studentNameSplit[1] ? studentNameSplit[1] : studentNameSplit[0]
+        const studentCourse = availableCourses.find(course => {
+            return course.id === registeredStudent.courseId;
+        });
+        const studentToCreate = {
+            firstName: studentFirstName,
+            lastName: studentLastName,
+            courseId: studentCourse.id
+        };
+        createNewRegisteredStudent(studentToCreate).then(res => {
+            console.log("Successfully create new registered student with id: " + res.data)
+        }).catch(err =>
+            console.log('Got an error when create new registered student: ', err));
+        setRegisteredStudents((prevStudents) => {
+            return [registeredStudent, ...prevStudents];
+        });
+    }
+
+    return (
+        <div>
+            {!availableCourses ? "Loading..." :
+                <div>
+                    <NewStudent onAddStudent={addStudentHandler} courses={availableCourses}/>
+                    <Registrations registeredStudents={registeredStudents} courses={availableCourses}/>
+                </div>
+            }
+        </div>
+    );
 }
 
 export default App;
